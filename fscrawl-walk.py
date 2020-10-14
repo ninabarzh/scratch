11c097de73c2aa225cdaf8aec7711d9b641a5ca6
Nina Barzh <30133030+ninabarzh@users.noreply.github.com>
2020-10-14 22:06:30 +0200

    Added argument parsing for start directory

--- fscrawl-walk.py
+++ fscrawl-walk.py
@@ -1,8 +1,14 @@
+import argparse
 import os
 
-for (dirpath, dirnames, filenames) in os.walk('.'):
-    for f in filenames:
-        print('File: ', os.path.join(dirpath, f))
-    for d in dirnames:
-        print('Directory: ', os.path.join(dirpath, d))
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--startdir", help="directory to start from", default='.')
+args = parser.parse_args()
+
+if args.start:
+    for (dirpath, dirnames, filenames) in os.walk(args.start):
+        for f in filenames:
+            print('File: ', os.path.join(dirpath, f))
+        for d in dirnames:
+            print('Directory: ', os.path.join(dirpath, d))
 
