27af92131842ae685af9eb41f166cc70ee8c0e3f
Nina Barzh <30133030+ninabarzh@users.noreply.github.com>
2020-10-15 23:22:21 +0200

    Templates dir on project level

--- find/find/settings.py
+++ find/find/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Templates Directory
+TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
