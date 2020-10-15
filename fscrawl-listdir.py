7d3e473465b87c97c551b3c31d0a0b11e9e6892b
Nina Barzh <30133030+ninabarzh@users.noreply.github.com>
2020-10-15 14:49:35 +0200

    Woharg, messing about

--- fscrawl-listdir.py
+++
@@ -1,5 +0,0 @@
-import os
-
-folder = '.'
-filepaths = [os.path.join(folder, f) for f in os.listdir(folder)]
-print (filepaths)
--- fscrawl-scandir.py
+++
@@ -1,7 +0,0 @@
-import os
-
-filepaths = [f.path for f in os.scandir('.') if f.is_file()]
-dirpaths = [f.path for f in os.scandir('.') if f.is_dir()]
-
-print(filepaths)
-print(dirpaths)
