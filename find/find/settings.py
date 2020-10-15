878c544f7e4aceca4b0b75011602a4ed0f07f041
Nina Barzh <30133030+ninabarzh@users.noreply.github.com>
2020-10-15 16:14:57 +0200

    Updated INSTALLED_APPS with applications just created

--- find/find/settings.py
+++ find/find/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'filesystem',
+    'web',
+    'network'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
