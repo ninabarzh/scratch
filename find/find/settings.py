f133e96e3dad9092c68ee931d3d8495d6cff4a61
Nina Barzh <30133030+ninabarzh@users.noreply.github.com>
2020-10-19 12:27:51 +0200

    Views and wiring urls

--- find/find/settings.py
+++ find/find/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,9 +35,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'filesystem',
-    'web',
-    'network'
+    'filesystem.apps.FilesystemConfig',
+    'web.apps.WebConfig',
+    'network.apps.NetworkConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- find/find/urls.py
+++ find/find/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('web', include('web.urls')),
+    path('network', include('network.urls')),
+    path('filesystem', include('filesystem.urls')),
     path('admin/', admin.site.urls),
 ]
