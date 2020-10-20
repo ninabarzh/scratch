2a71e03809d6d0f152502c10899061429a68d138
Nina Barzh <30133030+ninabarzh@users.noreply.github.com>
2020-10-21 00:07:38 +0200

    Messing with views

--- find/find/settings.py
+++ find/find/settings.py
@@ -16,9 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Templates Directory
-TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -61,7 +58,7 @@ ROOT_URLCONF = 'find.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
