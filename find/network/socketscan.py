8f9d7ce94c9883246559a84bd1f779eb75e4f84f
Nina Barzh <30133030+ninabarzh@users.noreply.github.com>
2020-10-22 21:46:45 +0200

    Redone is_valid_hostname for argparse

--- find/network/socketscan.py
+++ find/network/socketscan.py
@@ -3,28 +3,28 @@ Authors: Reinica and Nina
 Struggled with: https://bugs.python.org/issue32958
 Port scanning is like going to someone’s house and checking their doors and windows.
 If not by request of owners, use port scanners like this only on localhost or own website."""
+import argparse
 import socket
 import time
-import argparse
 import re
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Process command line arguments.')
-    parser.add_argument("-p", "--host", help="host to be scanned", type=is_valid_hostname, default='localhost')
+    parser.add_argument("-p", "--host", help="Host to be scanned", type=is_valid_hostname, default='localhost')
     return parser.parse_args()
 
 
-def is_valid_hostname(hostname):
+def is_valid_hostname(host):
     # A valid DNS name:
     #   has a maximum length of 253 characters
     #   consists only of allowed characters
     #   doesn't begin or end with a hyphen
 
-    if hostname.endswith('.'):                                      # A single trailing dot is legal
-        hostname = hostname[:-1]                                    # Strip a dot from the right
-    if len(hostname) < 1 or len(hostname) > 253:
-        raise argparse.ArgumentTypeError(f"{hostname} is not a valid hostname")
+    if host.endswith('.'):  # A single trailing dot is legal
+        host = host[:-1]  # Strip a dot from the right
+    if len(host) < 1 or len(host) > 253:
+        raise argparse.ArgumentTypeError(f"{host} is not a valid hostname")
 
     # Split by label and verify
     #   length is within proper range
@@ -32,9 +32,11 @@ def is_valid_hostname(hostname):
     #   does not contain disallowed characters
 
     disallowed = re.compile("[^A-Z\\d-]", re.IGNORECASE)
-    for label in hostname.split("."):
+    for label in host.split("."):
         if (len(label) > 63) or (label.startswith("-")) or (label.endswith("-")) or (disallowed.search(label)):
-            raise argparse.ArgumentTypeError(f"{hostname} is not a valid hostname")
+            raise argparse.ArgumentTypeError(f"{host} is not a valid hostname")
+        else:
+            return host
 
 
 def scan(host):
@@ -42,12 +44,14 @@ def scan(host):
     print('Starting scan on host: ', tip)
 
     for i in range(50, 500):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        connect = s.connect_ex((tip, i))
-        if connect == 0:
-            print('Port %d: OPEN' % (i,))
-        s.close()
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            connect = socket.create_connection((tip, i))
+            if connect == 0:
+                print('Port %d: OPEN' % (i,))
+            s.close()
+        except socket.error as err:
+            print("socket creation with ", tip, "failed with error %s" % err)
 
 
 def main():
