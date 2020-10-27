c547bdfeb635fa1a158e5e6410eeda696a4ca4f8
Nina Barzh <30133030+ninabarzh@users.noreply.github.com>
2020-10-27 16:20:29 +0100

    Switching to using postgreSQL

--- find/find/settings.py
+++ find/find/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+# import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'find.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scratch',
+        'USER': 'nina',
+        'PASSWORD': 'secret',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
